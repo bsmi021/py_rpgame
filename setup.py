d74e151188cb513c42edcdb187f658f1d588950c
bsmi021@gmail.com <bsmi021@gmail.com>
2018-11-22 07:35:36 -0500

    Updated values
    Signed-off-by: bsmi021@gmail.com <bsmi021@gmail.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,37 @@
-from setuptools import setup
+"""rpgame setup script."""
+from datetime import datetime as dt
+
+from setuptools import setup, find_packages
+
+PROJECT_NAME = 'MMORPG Demo App'
+PROJECT_PACKAGE_NAME = 'rpgame'
+PROJECT_LICENSE = 'Apached License 2.0'
+PROJECT_AUTHOR = 'Brian W. Smith'
+PROJECT_COPYRIGHT = ' 2018-{}, {}'.format(dt.now().year, PROJECT_AUTHOR)
+PROJECT_URL = ''
+PROJECT_EMAIL = ''
+
+PACKAGES = find_packages(exclude=['test', 'tests.*'])
+
+REQUIRES = [
+    'pytz>=2018.04',
+    'confluent_kafka'
+]
 
 setup(
-    name='rpgame',
+    name=PROJECT_NAME,
     version='1',
-    packages=['rpgame'],
-    url='',
-    license='',
-    author='Brian W. Smith',
-    author_email='bsmi021@gmail.com',
-    description='Open project used to simulate rpg game'
+    packages=PACKAGES,
+    url=PROJECT_URL,
+    license=PROJECT_LICENSE,
+    author=PROJECT_AUTHOR,
+    author_email=PROJECT_EMAIL,
+    include_package_data=True,
+    description='Open project used to simulate rpg game',
+    install_requires=REQUIRES,
+    entry_points={
+        'console_scripts': [
+            'rpggame = rpggame.__main__:main'
+        ]
+    }
 )
